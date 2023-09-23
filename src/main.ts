758739b4dda0b17837090cb12d8689174d2deb93
ZukTol <sldancer@yandex.ru>
2023-09-23 22:44:46 +0300

    show xml

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,9 @@ export default class LoaderPlugin extends Plugin {
 		if(this.settings.doLoadTxt)
 			this.registerExtensions(["json"], "markdown");
 
+		if (this.settings.doLoadXml)
+			this.registerExtensions(["xml"], "markdown");
+
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
