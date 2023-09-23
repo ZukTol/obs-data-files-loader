c85d30acfd10037f3e8191798dd6d67bb40cce31
ZukTol <sldancer@yandex.ru>
2023-09-23 20:59:26 +0300

    Initial commit

---
+++ src/loader-settings-tab.ts
@@ -0,0 +1,44 @@
+import { App, PluginSettingTab, Setting } from 'obsidian';
+import MyPlugin from './main'
+
+export default class LoaderSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Load .txt files')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.doLoadTxt)
+				.onChange(async (value) => {
+					this.plugin.settings.doLoadTxt = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Load .json files')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.doLoadJson)
+				.onChange(async (value) => {
+					this.plugin.settings.doLoadJson = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Load .xml files')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.doLoadXml)
+				.onChange(async (value) => {
+					this.plugin.settings.doLoadXml = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { Plugin } from 'obsidian';
+import LoaderSettingTab from './loader-settings-tab';
+
+// Remember to rename these classes and interfaces!
+
+interface MyPluginSettings {
+	doLoadTxt: boolean;
+	doLoadXml: boolean;
+	doLoadJson: boolean;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	doLoadTxt: true,
+	doLoadXml: true,
+	doLoadJson: true
+}
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		if(this.settings.doLoadTxt)
+			this.registerExtensions(["txt"], "markdown");
+
+		if(this.settings.doLoadTxt)
+			this.registerExtensions(["json"], "markdown");
+
+		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('Status Bar Text');
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new LoaderSettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+
