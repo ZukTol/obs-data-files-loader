afe87d1cfe87d57e17cb9c679f01e77d6f91c395
ZukTol <sldancer@yandex.ru>
2023-10-04 22:24:31 +0300

    work on json view

--- src/JsonView.ts
+++ src/JsonView.ts
@@ -1,16 +1,30 @@
 import { TextFileView, WorkspaceLeaf } from "obsidian";
 import { VIEW_TYPE_JSON } from './constants'
 import LoaderPlugin from "./main";
+import * as CodeMirror from "codemirror";
 
 export default class JsonView extends TextFileView {
 
 	public plugin: LoaderPlugin;
-	private cmEditor: any; 
+	private cmEditorFactory: any; 
+	private cmEditor: any | CodeMirror.Editor;
+	private editorEl: any;
 
 	constructor(leaf: WorkspaceLeaf, plugin: LoaderPlugin) {
 		super(leaf);
 		this.plugin = plugin;
-		this.cmEditor = window.CodeMirror;
+		this.cmEditorFactory = window.CodeMirror;
+	}
+
+	onload() {
+		super.onload();
+		this.editorEl = super.contentEl.createDiv("markdown-source-view mod-cm5");
+		this.cmEditor = this.cmEditorFactory(this.editorEl, {
+			mode: "hypermd",
+			theme: "obsidian",
+			lineWrapping: !0,
+			styleActiveLine: !0,
+		});
 	}
 
 	// gets the title of the document
@@ -25,7 +39,7 @@ export default class JsonView extends TextFileView {
 	}
 
 	getViewData(): string {
-		return "Sample data";
+		return this.cmEditor.getValue();
 	}
 
 	getViewType(): string {
@@ -33,7 +47,39 @@ export default class JsonView extends TextFileView {
 	}
 
 	setViewData(data: string, clear: boolean): void {
-		const a = 3+5;
+		const n = this.cmEditor;
+		// if (clear)
+		// {
+			n.setValue(data);
+			// n.clearHistory();
+			// this.lastCursor = null;
+		// }
+		// else {
+		// 	for (let i = n.getScrollInfo(), r = data.split(/\r?\n/g), o = n.lineCount(), a = 0; a < r.length && a < o && r[a] === n.getLine(a); )
+		// 		a++;
+		// 	if (a === r.length && a === o)
+		// 		return;
+		// 	for (var s = o - 1, l = r.length - 1; s > a && l > a && n.getLine(s) === r[l]; )
+		// 		s--,
+		// 			l--;
+		// 	var c = {
+		// 		line: a,
+		// 		ch: 0
+		// 	}
+		// 		, u = {
+		// 		line: s,
+		// 		ch: n.getLine(s).length
+		// 	}
+		// 		, h = r.slice(a, l + 1).join("\n");
+		// 	a === Math.min(o, r.length) && (c = {
+		// 		line: a - 1,
+		// 		ch: n.getLine(a - 1).length
+		// 	},
+		// 	r.length > o && (h = "\n" + h)),
+		// 		n.replaceRange(h, c, u, "setValue"),
+		// 		this.scrolling = !0,
+		// 		n.scrollTo(i.left, i.top)
+		// }
 	}
 
 }
